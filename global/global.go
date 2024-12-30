a227ff073e475e58013e8e03743b29e0c2c32b9e
dangquanghung <hungvuive2000@gmail.com>
2024-12-30 15:02:26 +0700

    connect mysql

--- global/global.go
+++ global/global.go
@@ -3,12 +3,13 @@ package global
 import (
 	"github.com/dangquanghung/go-ecommerce-backend-api/pkg/logger"
 	"github.com/dangquanghung/go-ecommerce-backend-api/pkg/setting"
+	"gorm.io/gorm"
 )
 
 var (
 	Config setting.Config
 	Logger *logger.LoggerZap
-	// Mdb *gorm.DB
+	Mdb    *gorm.DB
 )
 
 /*
--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -1,3 +1,59 @@
 package initialize
 
-func InitMysql() {}
+import (
+	"fmt"
+	"time"
+
+	"github.com/dangquanghung/go-ecommerce-backend-api/global"
+	"github.com/dangquanghung/go-ecommerce-backend-api/internal/po"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func checkErrorPanic(err error, errString string) {
+	if err != nil {
+		global.Logger.Error(errString, zap.Error(err))
+		panic(err)
+
+	}
+}
+func InitMysql() {
+	m := global.Config.Mysql
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	var s = fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Dbname)
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		SkipDefaultTransaction: false,
+	})
+	checkErrorPanic(err, "InitMysql initialization error")
+	global.Mdb = db
+
+	// set Pool
+	SetPool()
+	migrateTables()
+
+}
+
+func SetPool() {
+	m := global.Config.Mysql
+
+	sqlDB, err := global.Mdb.DB()
+	if err != nil {
+		fmt.Printf("mysql error:%s::", err)
+	}
+
+	sqlDB.SetConnMaxIdleTime(time.Duration(m.MaxIdleConns))
+	sqlDB.SetMaxOpenConns(m.MaxOpenConns)
+	sqlDB.SetConnMaxLifetime(time.Duration(m.ConnMaxLifetime))
+}
+
+func migrateTables() {
+	err := global.Mdb.AutoMigrate(
+		&po.User{},
+		&po.Role{},
+	)
+	if err != nil {
+		fmt.Println("Migrating table error:", err)
+	}
+}
