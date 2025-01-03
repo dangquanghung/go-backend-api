6216c7d681fdf48fe6bc0b9eed62e44f6afe6773
dangquanghung <hungvuive2000@gmail.com>
2025-01-03 14:18:14 +0700

    connect redis

--- global/global.go
+++ global/global.go
@@ -3,6 +3,7 @@ package global
 import (
 	"github.com/dangquanghung/go-ecommerce-backend-api/pkg/logger"
 	"github.com/dangquanghung/go-ecommerce-backend-api/pkg/setting"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
@@ -10,6 +11,7 @@ var (
 	Config setting.Config
 	Logger *logger.LoggerZap
 	Mdb    *gorm.DB
+	Rdb    *redis.Client
 )
 
 /*
--- internal/initialize/redis.go
+++ internal/initialize/redis.go
@@ -1,3 +1,32 @@
 package initialize
 
-func InitRedis() {}
+import (
+	"context"
+	"fmt"
+
+	"github.com/dangquanghung/go-ecommerce-backend-api/global"
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
+
+var ctx = context.Background()
+
+func InitRedis() {
+
+	r := global.Config.Redis
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%v", r.Host, r.Port),
+		Password: r.Password, // no password set
+		DB:       r.Database, // use default DB
+		PoolSize: r.Port,     // 10 connections per pool
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		global.Logger.Error("Redis initialization error:", zap.Error(err))
+	}
+
+	fmt.Println("Redis initialized successfully")
+	global.Rdb = rdb
+}
